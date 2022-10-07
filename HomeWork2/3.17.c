7d8397afe3c271c5a58aa5550ca47a4d347ffbbf
brian <0103.brian@gmail.com>
2022-10-07 14:47:58 +0800

    first question

--- HomeWork2/3.17.c
+++ HomeWork2/3.17.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int account, begin, charged, total_credits, limit;
 
 int Credit_Limit_Calculator() {
-	
+	printf("Enter account number: ");
+	scanf("%d", &account);
+	printf("Enter balance at the beginning of mouth");
+	scanf("%d", &begin);
+	printf("Enter total");
+	scanf("%d", total_credits);
 }
